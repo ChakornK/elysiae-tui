1852c4a41e364c9c040acdf20a78567ec30b1835
Chakorn Kungsanarux <chakornk2007@gmail.com>
2026-08-08 16:26:15 -0700

    fix: handle SIGTERM and force exit on second signal

--- src/signal.rs
+++ src/signal.rs
@@ -1,11 +1,28 @@
 use tokio::sync::watch;
 
-/// Spawns a background task that sets the channel to `true` on SIGINT (Ctrl+C).
+/// Spawns a background task that sets the channel to `true` on SIGINT or SIGTERM.
+/// A second signal forces immediate process exit.
 pub fn spawn_signal_handler() -> watch::Receiver<bool> {
     let (tx, rx) = watch::channel(false);
     tokio::spawn(async move {
-        tokio::signal::ctrl_c().await.ok();
+        use tokio::signal::unix::{signal, SignalKind};
+
+        let mut sigterm = signal(SignalKind::terminate()).ok();
+        let mut sigint = signal(SignalKind::interrupt()).ok();
+
+        // Wait for first signal
+        tokio::select! {
+            _ = async { sigint.as_mut().unwrap().recv().await }, if sigint.is_some() => {}
+            _ = async { sigterm.as_mut().unwrap().recv().await }, if sigterm.is_some() => {}
+        }
         let _ = tx.send(true);
+
+        // Second signal: force exit
+        tokio::select! {
+            _ = async { sigint.as_mut().unwrap().recv().await }, if sigint.is_some() => {}
+            _ = async { sigterm.as_mut().unwrap().recv().await }, if sigterm.is_some() => {}
+        }
+        std::process::exit(130);
     });
     rx
 }
